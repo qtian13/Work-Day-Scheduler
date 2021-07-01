f660aaeb25979434f47d8d98c48e8f469b31d6a0
qtian13 <qiushuang.tian@gmail.com>
2021-07-01 00:42:28 -0700

    finish the css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
 var dayToday = moment().format("dddd, MMMM Do");
+// get the calendar in Local Storage if there is
+var calendar = JSON.parse(localStorage.getItem("calendar"));
 
+// display the date today like in format like Thursday, July 1st
 $("#currentDay").text(dayToday);
-
-var calender = JSON.parse(localStorage.getItem("calender"));
+// rrun the function when document is loaded
 $(document).ready(function() {
-    if (calender == null || calender.date !== moment().format('MMMM Do YYYY')) {
-        calender = {};
-        calender.date = moment().format('MMMM Do YYYY');
+    // check if there is calendar of today stored before
+    if (calendar == null || calendar.date !== moment().format('MMMM Do YYYY')) {
+        // if not, assign an empty object to the calendar and set the date as today
+        calendar = {};
+        calendar.date = moment().format('MMMM Do YYYY');
     } else {
+        // if so, update the page content with the calendar
         $(".input-box").each(function() {
-            $(this).val(calender[$(this).attr("id")]);
+            $(this).val(calendar[$(this).attr("id")]);
         })
     }
+    // check each hour block is past, present or future and change the background color
     $(".time-block").each(function(i, element) {
         var currentTime = parseInt(moment().format("HH"));
-        if (i < currentTime) {
+        if (i + 8 < currentTime) {
             $(element).addClass("past text-black-50");
-            $(element).find("textarea").attr("readonly", "readonly");
             $(element).find("textarea").addClass("text-black-50");
-        } else if (i == currentTime) {
+        } else if (i + 8 == currentTime) {
             $(element).addClass("present");
         } else {
             $(element).addClass("future");
@@ -26,9 +31,9 @@ $(document).ready(function() {
     })
 });
 
-$(".save").on("click", function() {
+// save the textarea value in the calender and store the calender to Local Storage every time the save button is clicked
+$(".saveBtn").on("click", function() {
     var buttonID = "#" + $(this).attr("id");
-    calender[$(buttonID).prev().attr("id")] = $(buttonID).prev().val();
-    localStorage.setItem("calender", JSON.stringify(calender));
-    
+    calendar[$(buttonID).prev().attr("id")] = $(buttonID).prev().val();
+    localStorage.setItem("calendar", JSON.stringify(calendar));
 })
